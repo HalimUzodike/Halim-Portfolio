acaf21e5b95691fd9b57826bf4dbb2abbd916f89
Halim Uzodike <98881780+HalimUzodike@users.noreply.github.com>
2024-07-30 02:04:40 -0400

    Testing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/Halim-Portfolio",
     output: "export",
-    env: {
-        RESEND_API_KEY: process.env.RESEND_API_KEY,
-    },
+    basePath: '/Halim-Portfolio',
 };
 
 module.exports = nextConfig;
