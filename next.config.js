5d50f9a43f49953371131b49c3a3bacdecba9cd8
Halim Uzodike <98881780+HalimUzodike@users.noreply.github.com>
2024-07-30 01:56:46 -0400

    Changed config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 const nextConfig = {
     basePath: "/Halim-Portfolio",
     output: "export",
+    env: {
+        RESEND_API_KEY: process.env.RESEND_API_KEY,
+    },
 };
-module.exports = nextConfig
+
+module.exports = nextConfig;
