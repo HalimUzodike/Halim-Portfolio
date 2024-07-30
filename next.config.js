de3edf1c50b023432e8bdcfbf1a9bd2c103aee06
Halim Uzodike <98881780+HalimUzodike@users.noreply.github.com>
2024-07-30 00:53:48 -0400

    Attempt to deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    basePath: "/Halim-Portfolio",
+    output: "export",
+    // reactStrictMode: true,
+};
 module.exports = nextConfig
